b75483ed3742c264bc050ace5c1e37a69cba8266
TebogoMsimanga <st10488599@rcconnect.edu.za>
2025-05-26 13:17:16 +0200

    textfield modifiers

--- app/src/main/java/com/example/calculatordemo/Calculator.kt
+++ app/src/main/java/com/example/calculatordemo/Calculator.kt
@@ -1,2 +1,98 @@
 package com.example.calculatordemo
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Composable
+fun Calculator() {
+    var number1 by remember { mutableStateOf("") }
+    var number2 by remember { mutableStateOf("") }
+    var result by remember { mutableStateOf("") }
+
+    Box() {
+        Column {
+            TextField(
+                placeholder = {
+                    Text(
+                        text = "Enter First Digit"
+                    )
+
+                }
+
+
+            )
+            TextField(
+                placeholder = {
+                    Text(
+                        text = "Enter Second Digit"
+                    )
+
+                }
+            )
+            TextField(
+                placeholder = {
+                    Text(
+                        text = "Result"
+                    )
+
+                }
+            )
+        }
+        Row {
+            Button(
+                onClick = {}
+            ) {
+                Text(
+                    text = "+"
+                )
+            }
+            Button(
+                onClick = {}
+            ) {
+                Text(
+                    text = "-"
+                )
+            }
+            Button(
+                onClick = {}
+            ) {
+                Text(
+                    text = "*"
+                )
+
+                Button(
+                    onClick = {}
+                ) {
+                    Text(
+                        text = "/"
+                    )
+                }
+
+            }
+        }
+    }
+}
+
+
+
+
+    @Preview(showBackground = true)
+    @Composable
+    fun CalculatorPreview() {
+        Calculator()
+
+
+    }
+
+
